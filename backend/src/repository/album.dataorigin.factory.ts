213722aad780ed5b4525d004e0b8c71b0181dfc6
Alejandro Almunia Herranz <aalmunia@gmail.com>
2022-02-09 06:11:09 +0100

    Little touches OOK

--- backend/src/repository/album.dataorigin.factory.ts
+++ backend/src/repository/album.dataorigin.factory.ts
@@ -2,11 +2,11 @@
 Agnostic data factory, otherwise we would need to import EVERY SINGLE data origin here, 
 and add a humongous switch to the .getDataOrigin() method
 */
-import { albumDataOriginsMap } from "../model/album.dataorigin.types";
-import { albumDataOriginsKeys } from "../model/album.dataorigin.types";
-import { albumDataOriginsTypes } from "../model/album.dataorigin.types";
-import { ExtractInstanceType } from "../model/album.dataorigin.types";
-
+import { albumDataOriginsMap, 
+        albumDataOriginsKeys, 
+        albumDataOriginsTypes, 
+        ExtractInstanceType  } 
+from "../model/album.dataorigin.types";
 
 export default class AlbumDataOriginFactory {        
     public static getDataOrigin(key:albumDataOriginsKeys) : ExtractInstanceType<albumDataOriginsTypes> {
